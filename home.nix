b395e43a5fd6eb9f02a1ca69a7b8d3b5576896f7
sysarcher <sysarcher@users.noreply.github.com>
2024-10-25 14:54:40 +0200

    added jetbrains font

--- home.nix
+++ home.nix
@@ -8,7 +8,7 @@
 
   home.packages = with pkgs; [
     htop
-    (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+    (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" "JetBrainsMono" ]; })
     ripgrep
     neovim
     gcc
