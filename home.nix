a8770ed8ca5de13c3a54cabaabc3f73d287c8cde
sysarcher <sysarcher@users.noreply.github.com>
2024-10-25 06:49:51 -0400

    add initial home config

---
+++ home.nix
@@ -0,0 +1,94 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "taimoor";
+  home.homeDirectory = "/home/taimoor";
+
+  home.stateVersion = "24.05"; # Please read the comment before changing.
+
+  home.packages = with pkgs; [
+    htop
+    (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+    ripgrep
+    neovim
+    gcc
+    jujutsu
+    tree
+  ];
+  programs.gh = {
+    enable = true;
+    settings = {
+      git_protocol = "ssh";
+    };
+  };
+  programs.vim = {
+    enable = true;
+    defaultEditor = true;
+    plugins = with pkgs.vimPlugins; [
+      vim-nix
+      gruvbox
+      vim-airline
+    ];
+    extraConfig = "colorscheme desert";
+  };
+
+  programs.zsh = {
+      enable = true;
+      enableCompletion = true;
+      autosuggestion.enable = true;
+      syntaxHighlighting.enable = true;
+      shellAliases = {
+        k = "kubectl";
+      };
+      oh-my-zsh = {
+        enable = true;
+        plugins = [ "docker-compose" "docker" ];
+        theme = "dst";
+      };
+      initExtra = ''
+        bindkey '^f' autosuggest-accept
+      '';
+    };
+  
+    programs.fzf = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. These will be explicitly sourced when using a
+  # shell provided by Home Manager. If you don't want to manage your shell
+  # through Home Manager then you have to manually source 'hm-session-vars.sh'
+  # located at either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/taimoor/etc/profile.d/hm-session-vars.sh
+  #
+  home.sessionVariables = {
+    EDITOR = "vim";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
